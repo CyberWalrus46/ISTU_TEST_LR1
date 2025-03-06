41362f131f436092d702942990ffd2d965adfa05
CyberWalrus46 <153679137+CyberWalrus46@users.noreply.github.com>
2025-03-06 12:15:33 +0400

    Добавьте файлы проекта.

---
+++ Библия_56вар/Class1.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System;
+
+namespace TestLib
+{
+    public static class Replace
+    {
+        public static (List<int>, List<String>) ChangeListInt(List<Object> lst)
+        {
+            List<int> newLst = new List<int>();
+            List<String> errorLst = new List<String>();
+            for (int i = 0; i < lst.Count; i++)
+            {
+                int itemInt = new int();
+                if (int.TryParse(lst[i],out itemInt))
+                //try
+                //{
+                //    int itemInt = (int)lst[i];
+                //    if (itemInt < 0 && itemInt % 2 == 0)
+                //    {
+                //        int res = itemInt * itemInt;
+                //        newLst.Append(res);
+                //    }
+                //}
+                //catch (Exception ex) {
+                //{
+                //    errorLst.Append(ex.Message);  
+                //}
+            }
+            return (newLst, errorLst);
+        }
+    }
+}
