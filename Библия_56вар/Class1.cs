dd253bb04e6aacdb327f924485f535758427161c
CyberWalrus46 <153679137+CyberWalrus46@users.noreply.github.com>
2025-03-11 10:27:50 +0400

    Изменил код проекта и соответственно поправил код тестовсырой код и тесты

--- Библия_56вар/Class1.cs
+++ Библия_56вар/Class1.cs
@@ -4,31 +4,66 @@ using System;
 
 namespace TestLib
 {
-    public static class Replace
+    //public static class Replace
+    //{
+    //    public static (List<int>, List<String>) ChangeListInt(List<Object> lst)
+    //    {
+    //        List<int> newLst = new List<int>();
+    //        List<String> errorLst = new List<String>();
+    //        for (int i = 0; i < lst.Count; i++)
+    //        {
+    //            int itemInt = new int();
+    //            //if (int.TryParse(lst[i],out itemInt))
+    //            //try
+    //            //{
+    //            //    int itemInt = (int)lst[i];
+    //            //    if (itemInt < 0 && itemInt % 2 == 0)
+    //            //    {
+    //            //        int res = itemInt * itemInt;
+    //            //        newLst.Append(res);
+    //            //    }
+    //            //}
+    //            //catch (Exception ex) {
+    //            //{
+    //            //    errorLst.Append(ex.Message);  
+    //            //}
+    //        }
+    //        return (newLst, errorLst);
+    //    }
+    //}
+    public class ArrayProcessor
     {
-        public static (List<int>, List<String>) ChangeListInt(List<Object> lst)
+        public static (int[], long, List<string>) TransformArray(int[] inputArray)
         {
-            List<int> newLst = new List<int>();
-            List<String> errorLst = new List<String>();
-            for (int i = 0; i < lst.Count; i++)
+            if (inputArray == null)
+                return (Array.Empty<int>(), 0, new List<string> { "Input array is null." });
+
+            long product = 1;
+            List<string> errors = new List<string>();
+
+            foreach (var number in inputArray)
             {
-                int itemInt = new int();
-                if (int.TryParse(lst[i],out itemInt))
-                //try
-                //{
-                //    int itemInt = (int)lst[i];
-                //    if (itemInt < 0 && itemInt % 2 == 0)
-                //    {
-                //        int res = itemInt * itemInt;
-                //        newLst.Append(res);
-                //    }
-                //}
-                //catch (Exception ex) {
-                //{
-                //    errorLst.Append(ex.Message);  
-                //}
+                product *= number;
+                if (product == 0)
+                    break;
             }
-            return (newLst, errorLst);
+
+            for (int i = 0; i < inputArray.Length; i++)
+            {
+                if (inputArray[i] < 0 && inputArray[i] % 2 != 0)
+                {
+                    try
+                    {
+                        inputArray[i] = (inputArray[i] - (int)product) * (inputArray[i] + (int)product);
+                    }
+                    catch (OverflowException)
+                    {
+                        errors.Add($"Overflow occurred at index {i} for value {inputArray[i]}.");
+                    }
+                }
+            }
+
+            return (inputArray, product, errors);
         }
     }
 }
--- Старыгина_первая_попытка/UnitTest1.cs
+++ Старыгина_первая_попытка/UnitTest1.cs
@@ -1,11 +1,64 @@
-namespace Tests
+using Xunit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TestLib.Tests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
-        {
+        //int a = Replace(a);
+        //[Fact]
+        //public void Test1()
+        //{
 
+        //}
+        public class ArrayProcessorTests
+        {
+            [Fact]
+            public void TransformArray_ValidInput_ReturnsExpectedResult()
+            {
+                int[] input = { -3, -2, -1, 0, 1, 2, 3 };
+                var expectedArray = new int[] { 9, -2, 1, 0, 1, 2, 3 };
+                long expectedProduct = 0; // Product of -3, -2, -1
+                List<string> expectedErrors = new List<string>();
+
+                var result = ArrayProcessor.TransformArray(input);
+
+                Assert.Equal(expectedArray, result.Item1);
+                Assert.Equal(expectedProduct, result.Item2);
+                Assert.Equal(expectedErrors, result.Item3);
+            }
+
+            [Fact]
+            public void TransformArray_NullInput_ReturnsError()
+            {
+                int[] input = null;
+                var expectedArray = Array.Empty<int>();
+                long expectedProduct = 0;
+                List<string> expectedErrors = new List<string> { "Input array is null." };
+
+                var result = ArrayProcessor.TransformArray(input);
+
+                Assert.Equal(expectedArray, result.Item1);
+                Assert.Equal(expectedProduct, result.Item2);
+                Assert.Equal(expectedErrors, result.Item3);
+            }
+
+            [Fact]
+            public void TransformArray_NullInput_ReturnsError2()
+            {
+                int[] input = { -3, -2, 0, 0, 1, 2, 3 };
+                var expectedArray = new int[] { 4, -2, 4, 0, 1, 2, 3 };
+                long expectedProduct = 0; // Product of -3, -2, -1
+                List<string> expectedErrors = new List<string> { "Input array is null." };
+
+                var result = ArrayProcessor.TransformArray(input);
+
+                Assert.Equal(expectedArray, result.Item1);
+                Assert.Equal(expectedProduct, result.Item2);
+                Assert.Equal(expectedErrors, result.Item3);
+            }
         }
     }
 }
