123c0634d4e0df51e9b0b03ffcea6a9a089f6672
CyberWalrus46 <153679137+CyberWalrus46@users.noreply.github.com>
2025-03-11 11:20:09 +0400

    Добавил тестов, подчистил комментарии

--- Старыгина_первая_попытка/UnitTest1.cs
+++ Старыгина_первая_попытка/UnitTest1.cs
@@ -7,12 +7,6 @@ namespace TestLib.Tests
 {
     public class UnitTest1
     {
-        //int a = Replace(a);
-        //[Fact]
-        //public void Test1()
-        //{
-
-        //}
         public class ArrayProcessorTests
         {
             [Fact]
@@ -20,7 +14,7 @@ namespace TestLib.Tests
             {
                 int[] input = { -3, -2, -1, 0, 1, 2, 3 };
                 var expectedArray = new int[] { 9, -2, 1, 0, 1, 2, 3 };
-                long expectedProduct = 0; // Product of -3, -2, -1
+                long expectedProduct = 0; // Product of 0
                 List<string> expectedErrors = new List<string>();
 
                 var result = ArrayProcessor.TransformArray(input);
@@ -46,12 +40,57 @@ namespace TestLib.Tests
             }
 
             [Fact]
-            public void TransformArray_NullInput_ReturnsError2()
+            public void TransformArray_ValidInput_ReturnsExpectedResult_OnlyNegativeValue()
             {
-                int[] input = { -3, -2, 0, 0, 1, 2, 3 };
-                var expectedArray = new int[] { 4, -2, 4, 0, 1, 2, 3 };
-                long expectedProduct = 0; // Product of -3, -2, -1
-                List<string> expectedErrors = new List<string> { "Input array is null." };
+                int[] input = { -3, -2, -1 };
+                var expectedArray = new int[] { -27, -2, -35 };
+                long expectedProduct = -6; // Product of -3, -2, -1
+                List<string> expectedErrors = new List<string>();
+
+                var result = ArrayProcessor.TransformArray(input);
+
+                Assert.Equal(expectedArray, result.Item1);
+                Assert.Equal(expectedProduct, result.Item2);
+                Assert.Equal(expectedErrors, result.Item3);
+            }
+
+            [Fact]
+            public void TransformArray_ValidInput_ReturnsExpectedResult_OnlyPositiveValue()
+            {
+                int[] input = { 3, 2, 1 };
+                var expectedArray = new int[] { 3, 2, 1 };
+                long expectedProduct = 6; // Product of 3, 2, 1
+                List<string> expectedErrors = new List<string>();
+
+                var result = ArrayProcessor.TransformArray(input);
+
+                Assert.Equal(expectedArray, result.Item1);
+                Assert.Equal(expectedProduct, result.Item2);
+                Assert.Equal(expectedErrors, result.Item3);
+            }
+
+            [Fact]
+            public void TransformArray_ValidInput_ReturnsExpectedResult_OneNegativeValue()
+            {
+                int[] input = { -3};
+                var expectedArray = new int[] { 0 };
+                long expectedProduct = -3; // Product of -3
+                List<string> expectedErrors = new List<string>();
+
+                var result = ArrayProcessor.TransformArray(input);
+
+                Assert.Equal(expectedArray, result.Item1);
+                Assert.Equal(expectedProduct, result.Item2);
+                Assert.Equal(expectedErrors, result.Item3);
+            }
+
+            [Fact]
+            public void TransformArray_ValidInput_ReturnsExpectedResult_OnePositiveValue()
+            {
+                int[] input = { 3 };
+                var expectedArray = new int[] { 3 };
+                long expectedProduct = 3; // Product of 3
+                List<string> expectedErrors = new List<string>();
 
                 var result = ArrayProcessor.TransformArray(input);
 
