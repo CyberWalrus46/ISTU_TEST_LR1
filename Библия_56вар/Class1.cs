591d44a96604f7e31d1cbf69f1034b95efc3ddaa
CyberWalrus46 <153679137+CyberWalrus46@users.noreply.github.com>
2025-04-10 00:45:42 +0400

    Merge branch 'master' of https://github.com/CyberWalrus46/ISTU_TEST_LR1

--- Библия_56вар/Class1.cs
+++ Библия_56вар/Class1.cs
@@ -26,15 +26,9 @@ namespace TestLib
             {
                 try
                 {
-                    checked { product = checked(product * inputArray[i]);  }
-                    //product = checked(product * inputArray[i]);
-                    //long a = product * inputArray[i];
-                    //if (a > -2147483648 && a < 2147483647)
-                    //    product = (int)a;
-                    //else
-                    //    throw new OverflowException();
+                    product = checked(product * inputArray[i]);
                 }
-                catch(OverflowException) 
+                catch (OverflowException)
                 {
                     product = 0;
                     errors.Add($"The product overflowed when multiplied with {inputArray[i]} at index {i}. The product is set to 0.");
@@ -48,13 +42,7 @@ namespace TestLib
                 {
                     try
                     {
-                        checked { inputArray[i] = (inputArray[i] - product) * (inputArray[i] - product); }
-                        //inputArray[i] = checked((inputArray[i] - product) * (inputArray[i] - product));
-                        //long a = (inputArray[i] - product) * (inputArray[i] - product);
-                        //if (a < -2147483648 || a > 2147483647)
-                        //    throw new OverflowException();
-                        //else
-                        //    inputArray[i] = (int)a;
+                        inputArray[i] = (inputArray[i] - product) * (inputArray[i] - product);
                     }
                     catch (OverflowException)
                     {
